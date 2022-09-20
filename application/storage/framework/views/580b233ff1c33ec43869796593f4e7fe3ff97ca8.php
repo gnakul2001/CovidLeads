a9490134b59c4c6c31a2c11129fc807f5cca28e4
Nakul Gupta <gnakul2001@gmail.com>
2022-09-20 15:59:01 +0530

    Initial commit

---
+++ application/storage/framework/views/580b233ff1c33ec43869796593f4e7fe3ff97ca8.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>COVIN</title>
+    </head>
+    <body>
+        Hey the_nakulgupta.
+    </body>
+</html>
+<?php /**PATH /Users/nakulgupta/Github/NakulGupta/CovidLeads/application/resources/views/welcome.blade.php ENDPATH**/ ?>
